82ae998b83226a15a043f87b4a80a9007a54799f
ge78rih <ge78rih@tum.de>
2023-05-01 21:36:53 +0200

    ex2.1

---
+++ ex2.1.R
@@ -0,0 +1,11 @@
+x <- runif(100, 1, 100)
+x
+mean_x <- round(mean(x), 2)
+mean_x
+?runif
+
+runif(10, 5, 10)
+# generates random deviates of the uniform distribution
+# runif(n, min, max)
+# the mean_x change because of x value 
+
