483b4af67a9dedb66bfb546e9aa1b78e1097a943
ge78rih <ge78rih@tum.de>
2023-05-01 22:39:04 +0200

    no 2

--- ex2.1.R
+++ ex2.1.R
@@ -1,11 +1,24 @@
+## Exercise 2.1 ##
+
 x <- runif(100, 1, 100)
 x
 mean_x <- round(mean(x), 2)
 mean_x
 ?runif
-
-runif(10, 5, 10)
 # generates random deviates of the uniform distribution
 # runif(n, min, max)
 # the mean_x change because of x value 
+set.seed(100)
 
+
+## Exercise 2.2 ##
+# 1. vector x that entails all integers between 1 and 1000
+x <- (1:1000)
+x <- seq(1,1000,1)
+y <- 1000 - x
+y
+y <- x+y
+y
+
+
+## 
