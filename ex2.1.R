7c366c7ef6362d0358fc7ffdd0c3edc4fa795d7c
ge78rih <ge78rih@tum.de>
2023-05-09 10:46:10 +0200

    commit

--- ex2.1.R
+++ ex2.1.R
@@ -24,7 +24,9 @@ y
 ## Exercise 2.3 ##
 n <- 1e4
 scale <- 1.5e4
-income <- round( rbeta(n=n, shape1=2, shape2=12) * scale, 2)
+income <- round( rbeta(n=n, shape1=8, shape2=2) * scale, 2)
+# yes it looks realistic with 15000 and we try to adjust the shape 
+# skew change to the other side but still symmestric 
 
 install.packages('ggplot2')
 library(ggplot2)
@@ -62,5 +64,68 @@ income_summary
 ## 2 Lower 50%     1865  1073
 ## 3   Top 10%     4014  4979
 ## 4    Top 1%     6125  6737
+
+## 4.
+n <- 1e4
+scale <- 1.5e4
+income <- round( rbeta(n=n, shape1 = 2, shape2 = 12) * scale, 2)
+
+income_s <- sort(income)
+
+group <- c("Lower 1%", "Lower 50%", "Top 10%", "Top 1%")
+p <- c(.1, .5, .9, .99)
+
+boundary <- round(income_s[p*n], 0)
+
+low10_m <- mean( income_s[c(1:(.1*n))] )
+low50_m <- mean( income_s[c(1:(.5*n))] )
+top10_m <- mean( income_s[c((.9*n):n)] )
+top1_m <- mean( income_s[c((.99*n):n)] )
+
+means <-  round( c(low10_m, low50_m, top10_m, top1_m) , 0)
+
+income_summary <- data.frame(group, boundary, means)
+income_summary
+
+# group boundary means
+# 1  Lower 1%      149   118
+# 2 Lower 50%      284   200
+# 3   Top 10%      472   539
+# 4    Top 1%      610   670
+
+## 4) from the summary -> ???
+
+
+## Exercise 3: Working with data frames ## 
+dat <- data.frame(v1 = c(1,2,3,4,5),
+                  v2 = c(6,7,8,9,10),
+                  v3 = c(11,12,13,14,15),
+                  v4 = c(16,17,18,19,20),
+                  v5 = c(21,22,23,24,25))
+dat
+dat_mean <- mean(dat[c(1:5)])
+dat_mean
+
+
+install.packages('tidyverse')
+library(tidyverse)
+names(diamonds)
+view(diamonds)
+head(diamonds)
+
+?which
+diamons_cut <- diamonds[, "cut" == 'ideal']
+diamonds[which(diamons_cut)]
+
+
+
+
+
+
+
+
+
+
+
 
 
